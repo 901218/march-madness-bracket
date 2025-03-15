790ff68f1c2e47e51b2de2bc6dba8f896a320acc
901218 <Smehlert@Gmail.com>
2025-03-15 13:36:30 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALI9P3VSOuUmjlXo5VKBdSARQAH_6umNg",
+  authDomain: "march-madness-bracket-b24db.firebaseapp.com",
+  projectId: "march-madness-bracket-b24db",
+  storageBucket: "march-madness-bracket-b24db.firebasestorage.app",
+  messagingSenderId: "386065542708",
+  appId: "1:386065542708:web:0104ae67bd2c232a0da461"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
